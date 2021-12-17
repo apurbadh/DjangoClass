4fd5b84d43adf95eb0774923adaab2e86b5f81f2
apurbadh <erikduke182@gmail.com>
2021-12-17 17:08:35 +0545

    Classwork Done

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('product_list', views.product_list, name="product_list"),
     path('student/<int:roll>/<str:name>', views.product),
-    path('product_form', views.student_form, name="student_form")
+    path('product_form', views.student_form, name="student_form"),
+    path('product', views.products, name="product"),
+    path('productshow', views.product_list, name="productshow"),
+    path('productedit/<int:id>', views.productedit, name="productedit"),
+    path('productdestory/<int:id>', views.productdestroy, name="productdestroy"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
+from django.http import request
 from django.shortcuts import redirect, render 
 from .models import Shop, Product, Deal, Student
 from django.contrib import messages
+from myapp.forms import ProductForm
+
 
 # Create your views here.
 def index(req):
@@ -37,3 +40,40 @@ def student_form(req):
         return redirect('student_form');
         
     return render(req, "myapp/form.html")
+
+def products(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Created sucessfully")
+            return redirect('/product')
+        else:
+            messages.error(request, "Cannot store data")
+            return redirect("/product")
+    form = ProductForm()
+    return render(request, "myapp/product_new.html",{
+        "form" : form
+    } )
+ 
+
+def productedit(req, id):
+    if req.method == "POST":
+        form = ProductForm(req.POST, req.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Created sucessfully")
+            return redirect('/productedit')
+        else:
+            messages.error(request, "Cannot store data")
+            return redirect("/productedit")
+    product = Product.objects.get(id=id)
+    form = ProductForm(instance=product)
+    return render(req, "myapp/product_edit.html", {"form" : form})
+
+def productdestroy(req, id):
+   if request.method == "POST":
+       product = Product.objects.get(id=id)
+       product.delete()
+    
+   return redirect('/productshow') 
